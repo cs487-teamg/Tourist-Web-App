b2344a1b2287de1611f6e0166959592eecd572b7
Sufyan Menk <smenk@hawk.iit.edu>
2016-04-07 02:04:53 -0500

    left list populated & custom map pins added

--- js/map.js
+++ js/map.js
@@ -6,7 +6,10 @@ function initMap() {
 
   map = new google.maps.Map(document.getElementById('map'), {
     center: pyrmont,
-    zoom: 17
+    zoom: 17,
+    scaleControl: false,
+    streetViewControl: false,
+    mapTypeControl: false
   });
 
   infowindow = new google.maps.InfoWindow();
@@ -14,14 +17,16 @@ function initMap() {
   service.nearbySearch({
     location: pyrmont,
     radius: 500,
-    type: ['store']
+    type: ['tourist','store']
   }, callback);
 }
 
 function callback(results, status) {
+  var placesList = document.getElementById('places');
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     for (var i = 0; i < results.length; i++) {
       createMarker(results[i]);
+      placesList.innerHTML += '<button class="list-group-item"><h2 class="list-item">' + results[i].name + '</h2></button>';
     }
   }
 }
@@ -30,7 +35,8 @@ function createMarker(place) {
   var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
     map: map,
-    position: place.geometry.location
+    position: place.geometry.location,
+    icon: 'images/pins-dark/star-3.png'
   });
 
   google.maps.event.addListener(marker, 'click', function() {
